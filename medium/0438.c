faad59f6b4bc574365af39fd64ad69cc8193e11a
unknown <huangsihan.cn@gmail.com>
2021-02-02 15:44:04 +0800

    添加一道题

--- medium/0438.c
+++ medium/0438.c
@@ -21,14 +21,53 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define HASH_MAX 127
+
 int* findAnagrams(char* s, char* p, int* returnSize){
-    int idx_lth = 0;
+    int s_lth = strlen(s);
+    int p_lth = strlen(p);
+    int res_lth = 0;
+    int res[20101] = {0}; //结果集
 
+    int need_size = 0;
+    int need[HASH_MAX] = {0};
+    int window[HASH_MAX] = {0};
 
+    for (int i=0;i<p_lth;i++) need[p[i]]++;
+    for (int i=0;i<HASH_MAX;i++) if (need[i]) need_size++;
 
-    *returnSize = idx_lth;
-    int* idx_list = (int*)malloc(idx_lth*sizeof(int));
+    int left = 0, right = 0;
+    int valid = 0;
 
+    int start = 0, len = __INT_MAX__;
+
+    while (right<s_lth){
+        char c = s[right];
+        right++;
+        if(need[c]){
+            window[c]++;
+            if(window[c]==need[c]) valid++;
+        }
+
+        while (right-left>=p_lth){
+            if (valid==need_size){
+                res[res_lth] = left;
+                res_lth++;
+            }
+            char d = s[left];
+
+            left++;
+            if (need[d]){
+                if (window[d]==need[d]) valid--;
+                window[d]--;
+            }
+        }
+
+    }
+
+    *returnSize = res_lth;
+    int* idx_list = (int*)malloc(res_lth*sizeof(int));
+    for (int i=0;i<res_lth;i++)idx_list[i] = res[i];
 
     return idx_list;
 }
