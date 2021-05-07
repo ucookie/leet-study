59c07ada7410fe8accbdce842f30c750ef3f486e
huangsihan <huangsihan.cn@gmail.com>
2021-05-07 22:40:40 +0800

    添加一题

---
+++ simple/0026.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	tmp := root.Left
+	root.Left = root.Right
+	root.Right = tmp
+
+	invertTree(root.Left)
+	invertTree(root.Right)
+
+	return root
+}
+
+func print(root *TreeNode) {
+	if root == nil {
+		return
+	}
+
+	print(root.Left)
+	fmt.Printf("%d, ", root.Val)
+	print(root.Right)
+}
+
+func main() {
+	root := &TreeNode{
+		Val: 4,
+		Left: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 1,
+			},
+			Right: &TreeNode{
+				Val: 3,
+			},
+		},
+		Right: &TreeNode{
+			Val: 7,
+			Left: &TreeNode{
+				Val: 6,
+			},
+			Right: &TreeNode{
+				Val: 9,
+			},
+		},
+	}
+
+	print(root)
+	aa := invertTree(root)
+	fmt.Println("")
+	print(aa)
+}
